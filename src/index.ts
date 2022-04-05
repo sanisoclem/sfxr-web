9b60fd1e0a4b645c06279431119faef6f0bdeb43
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2022-04-05 22:46:49 +1000

    wasm audio test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,46 @@
-//@ts-ignore
-import init, { greet } from 'sfxr-rs';
+import init, { FmOsc } from 'sfxr-rs'
 
-init().then(() => {
-  console.log('init wasm-pack');
-  greet('test');
-});
+init()
+  .then((m: any) => {
+    console.log('init wasm-pack')
+    let fm = null
+
+    const play_button = document.getElementById('play')
+    play_button.addEventListener('click', (_event) => {
+      if (fm === null) {
+        fm = new m.FmOsc()
+        fm.set_note(50)
+        fm.set_fm_frequency(0)
+        fm.set_fm_amount(0)
+        fm.set_gain(0.8)
+      } else {
+        fm.free()
+        fm = null
+      }
+    })
+
+    const primary_slider = document.getElementById('primary_input')
+    primary_slider.addEventListener('input', (event) => {
+      const target = event.target as HTMLInputElement;
+      if (fm) {
+        fm.set_note(parseInt(target.value))
+      }
+    })
+
+    const fm_freq = document.getElementById('fm_freq')
+    fm_freq.addEventListener('input', (event) => {
+      const target = event.target as HTMLInputElement;
+      if (fm) {
+        fm.set_fm_frequency(parseFloat(target.value))
+      }
+    })
+
+    const fm_amount = document.getElementById('fm_amount')
+    fm_amount.addEventListener('input', (event) => {
+      const target = event.target as HTMLInputElement;
+      if (fm) {
+        fm.set_fm_amount(parseFloat(target.value))
+      }
+    })
+  })
+  .catch(console.error)
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-import wasmPack from 'vite-plugin-wasm-pack';
 
 export default defineConfig({
   build: {
     minify: false
   },
-  plugins: [wasmPack(['./sfxr-rs'])]
+  plugins: []
 });
