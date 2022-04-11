54a9ef7c30b9504f6e14abd94e5bc103920074d7
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2022-04-12 00:22:46 +1000

    use fork of fork of sfxr

--- sfxr-rs/src/lib.rs
+++ sfxr-rs/src/lib.rs
@@ -1,5 +1,8 @@
+use rand::rngs::SmallRng;
+use rand::RngCore;
+use rand::SeedableRng;
 use wasm_bindgen::prelude::*;
-use web_sys::{AudioContext, OscillatorType};
+use web_sys::{AudioContext};
 
 /// Converts a midi note to frequency
 ///
@@ -9,131 +12,50 @@ pub fn midi_to_freq(note: u8) -> f32 {
 }
 
 #[wasm_bindgen]
-pub struct FmOsc {
+pub struct EffectGenerator {
     ctx: AudioContext,
-    /// The primary oscillator.  This will be the fundamental frequency
-    primary: web_sys::OscillatorNode,
-
-    /// Overall gain (volume) control
-    gain: web_sys::GainNode,
-
-    /// Amount of frequency modulation
-    fm_gain: web_sys::GainNode,
-
-    /// The oscillator that will modulate the primary oscillator's frequency
-    fm_osc: web_sys::OscillatorNode,
-
-    /// The ratio between the primary frequency and the fm_osc frequency.
-    ///
-    /// Generally fractional values like 1/2 or 1/4 sound best
-    fm_freq_ratio: f32,
-
-    fm_gain_ratio: f32,
+    sample: sfxr::Sample,
 }
-
-impl Drop for FmOsc {
-    fn drop(&mut self) {
-        let _ = self.ctx.close();
-    }
-}
-
 #[wasm_bindgen]
-impl FmOsc {
+impl EffectGenerator {
     #[wasm_bindgen(constructor)]
-    pub fn new() -> Result<FmOsc, JsValue> {
+    pub fn new() -> Result<EffectGenerator, JsValue> {
         let ctx = web_sys::AudioContext::new()?;
-
-        // Create our web audio objects.
-        let primary = ctx.create_oscillator()?;
-        let fm_osc = ctx.create_oscillator()?;
-        let gain = ctx.create_gain()?;
-        let fm_gain = ctx.create_gain()?;
-
-        // Some initial settings:
-        primary.set_type(OscillatorType::Sine);
-        primary.frequency().set_value(440.0); // A4 note
-        gain.gain().set_value(0.0); // starts muted
-        fm_gain.gain().set_value(0.0); // no initial frequency modulation
-        fm_osc.set_type(OscillatorType::Sine);
-        fm_osc.frequency().set_value(0.0);
-
-        // Connect the nodes up!
-
-        // The primary oscillator is routed through the gain node, so that
-        // it can control the overall output volume.
-        primary.connect_with_audio_node(&gain)?;
-
-        // Then connect the gain node to the AudioContext destination (aka
-        // your speakers).
-        gain.connect_with_audio_node(&ctx.destination())?;
-
-        // The FM oscillator is connected to its own gain node, so it can
-        // control the amount of modulation.
-        fm_osc.connect_with_audio_node(&fm_gain)?;
-
-        // Connect the FM oscillator to the frequency parameter of the main
-        // oscillator, so that the FM node can modulate its frequency.
-        fm_gain.connect_with_audio_param(&primary.frequency())?;
-
-        // Start the oscillators!
-        primary.start()?;
-        fm_osc.start()?;
-
-        Ok(FmOsc {
+        Ok(EffectGenerator {
             ctx,
-            primary,
-            gain,
-            fm_gain,
-            fm_osc,
-            fm_freq_ratio: 0.0,
-            fm_gain_ratio: 0.0,
+            sample: sfxr::Sample::pickup(Some(SmallRng::from_entropy().next_u64())),
         })
     }
 
-    /// Sets the gain for this oscillator, between 0.0 and 1.0.
     #[wasm_bindgen]
-    pub fn set_gain(&self, mut gain: f32) {
-        if gain > 1.0 {
-            gain = 1.0;
-        }
-        if gain < 0.0 {
-            gain = 0.0;
-        }
-        self.gain.gain().set_value(gain);
+    pub fn play(&mut self) -> Result<(), JsValue> {
+        let generator = sfxr::Generator::new(self.sample.clone());
+        let buffer = generator.into_iter().collect::<Vec<_>>();
+        let sample_rate = self.ctx.sample_rate();
+        let abuffer = self
+            .ctx
+            .create_buffer(1, buffer.len() as u32, sample_rate)?;
+        abuffer.copy_to_channel(&buffer, 0)?;
+        let src = self.ctx.create_buffer_source()?;
+        src.set_buffer(Some(&abuffer));
+        src.connect_with_audio_node(&self.ctx.destination())?;
+        src.start()?;
+        Ok(())
     }
 
     #[wasm_bindgen]
-    pub fn set_primary_frequency(&self, freq: f32) {
-        self.primary.frequency().set_value(freq);
-
-        // The frequency of the FM oscillator depends on the frequency of the
-        // primary oscillator, so we update the frequency of both in this method.
-        self.fm_osc.frequency().set_value(self.fm_freq_ratio * freq);
-        self.fm_gain.gain().set_value(self.fm_gain_ratio * freq);
+    pub fn mutate(&mut self) -> Result<(), JsValue> {
+        self.sample
+            .mutate(Some(SmallRng::from_entropy().next_u64()));
+        self.play()
     }
 
-    #[wasm_bindgen]
-    pub fn set_note(&self, note: u8) {
-        let freq = midi_to_freq(note);
-        self.set_primary_frequency(freq);
+    pub fn randomize() {
+        unimplemented!()
     }
-
-    /// This should be between 0 and 1, though higher values are accepted.
-    #[wasm_bindgen]
-    pub fn set_fm_amount(&mut self, amt: f32) {
-        self.fm_gain_ratio = amt;
-
-        self.fm_gain
-            .gain()
-            .set_value(self.fm_gain_ratio * self.primary.frequency().value());
-    }
-
-    /// This should be between 0 and 1, though higher values are accepted.
-    #[wasm_bindgen]
-    pub fn set_fm_frequency(&mut self, amt: f32) {
-        self.fm_freq_ratio = amt;
-        self.fm_osc
-            .frequency()
-            .set_value(self.fm_freq_ratio * self.primary.frequency().value());
+}
+impl Drop for EffectGenerator {
+    fn drop(&mut self) {
+        let _ = self.ctx.close();
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,14 @@
-import init, { FmOsc } from 'sfxr-rs'
+import init, { EffectGenerator } from 'sfxr-rs'
 
 init()
   .then((m: any) => {
     console.log('init wasm-pack')
-    let fm = null
+    let gen = new EffectGenerator();
 
     const play_button = document.getElementById('play')
     play_button.addEventListener('click', (_event) => {
-      if (fm === null) {
-        fm = new FmOsc()
-        fm.set_note(50)
-        fm.set_fm_frequency(0)
-        fm.set_fm_amount(0)
-        fm.set_gain(0.8)
-      } else {
-        fm.free()
-        fm = null
-      }
-    })
-
-    const primary_slider = document.getElementById('primary_input')
-    primary_slider.addEventListener('input', (event) => {
-      const target = event.target as HTMLInputElement;
-      if (fm) {
-        fm.set_note(parseInt(target.value))
-      }
-    })
-
-    const fm_freq = document.getElementById('fm_freq')
-    fm_freq.addEventListener('input', (event) => {
-      const target = event.target as HTMLInputElement;
-      if (fm) {
-        fm.set_fm_frequency(parseFloat(target.value))
-      }
-    })
-
-    const fm_amount = document.getElementById('fm_amount')
-    fm_amount.addEventListener('input', (event) => {
-      const target = event.target as HTMLInputElement;
-      if (fm) {
-        fm.set_fm_amount(parseFloat(target.value))
+      if (gen !== null) {
+        gen.mutate();
       }
     })
   })
