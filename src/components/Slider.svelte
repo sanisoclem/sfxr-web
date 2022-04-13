256bba7d976830beb6b97a1101dbc252e28dddc0
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2022-04-14 01:01:52 +1000

    sloiders

---
+++ src/components/Slider.svelte
@@ -0,0 +1,50 @@
+<script context="module">
+  let counter = 0;
+</script>
+
+<script lang="ts">
+  export let label;
+  export let min = undefined;
+  export let max = undefined;
+  export let step = undefined;
+  export let value = 0;
+  export let units = undefined;
+</script>
+
+<style>
+  input[type='range'] {
+    @apply appearance-none w-full h-6 p-0 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none;
+  }
+
+  input[type='range']::-webkit-slider-runnable-track {
+    @apply w-full h-4 cursor-pointer bg-gray-300;
+  }
+  input[type='range']::-webkit-slider-thumb {
+    @apply shadow-none bg-white h-6 w-3 cursor-pointer rounded-none border-none;
+  }
+  input[type='range']:focus::-webkit-slider-runnable-track {
+    @apply bg-gray-600;
+  }
+  input[type='range']:focus::-webkit-slider-thumb {
+    @apply  bg-rose-500;
+  }
+
+  input[type='range']::-moz-range-track {
+    @apply w-full h-1 cursor-pointer bg-gray-600;
+  }
+  input[type='range']::-moz-range-thumb {
+    @apply shadow-none bg-white h-6 w-3 cursor-pointer rounded-none border-none;
+  }
+  input[type='range']:focus::-moz-range-track {
+    @apply bg-gray-600;
+  }
+  input[type='range']:focus::-moz-range-thumb {
+    @apply  bg-rose-500;
+  }
+</style>
+
+<div class="relative pt-1">
+  <label for={`slider${counter}`}>{label}</label>
+  <input id={`slider${counter}`} type="range" {min} {max} {step} bind:value={value} />
+  {value} {units || ''}
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,11 @@
 <script context="module" lang="ts">
-	export const prerender = true;
+  export const prerender = true;
 </script>
 
 <script lang="ts">
   import init, { EffectGenerator } from 'sfxr-web';
   import Button from '../components/Button.svelte';
+  import Slider from '../components/Slider.svelte';
   import { onMount } from 'svelte';
 
   let generator: EffectGenerator;
@@ -14,33 +15,65 @@
     generator = new EffectGenerator();
   });
 
-
   const randomize = (name: string) => () => {
     if (!generator) return;
     generator.randomize(name);
-  }
+  };
 
   function mutate() {
     if (!generator) return;
     generator.mutate();
-    console.log("mutating")
+    console.log('mutating');
   }
 </script>
 
 <svelte:head>
-	<title>Sound Effect Generator</title>
+  <title>Sound Effect Generator</title>
 </svelte:head>
 
 <div class="text-white bg-gray-900 min-h-screen flex flex-col justify-center items-center">
-	<h1 class="mb-8 text-2xl text-center w-full">Sound Effect Generator</h1>
-  <div class="w-full flex flex-row justify-center gap-4 flex-wrap">
-    <Button on:click={mutate}>Mutate</Button>
-    <Button on:click={randomize("pickup")}>Pickup</Button>
-    <Button on:click={randomize("laser")}>Laser</Button>
-    <Button on:click={randomize("explosion")}>Explosion</Button>
-    <Button on:click={randomize("powerup")}>Powerup</Button>
-    <Button on:click={randomize("hit")}>Hit</Button>
-    <Button on:click={randomize("jump")}>Jump</Button>
-    <Button on:click={randomize("blip")}>Blip</Button>
+  <div class="max-w-screen-lg p-4">
+    <h1 class="mb-8 text-2xl text-center w-full">Sound Effect Generator</h1>
+    <div class="w-full flex flex-row justify-center gap-4 flex-wrap">
+      <Button on:click={mutate}>Mutate</Button>
+      <Button on:click={randomize('pickup')}>Pickup</Button>
+      <Button on:click={randomize('laser')}>Laser</Button>
+      <Button on:click={randomize('explosion')}>Explosion</Button>
+      <Button on:click={randomize('powerup')}>Powerup</Button>
+      <Button on:click={randomize('hit')}>Hit</Button>
+      <Button on:click={randomize('jump')}>Jump</Button>
+      <Button on:click={randomize('blip')}>Blip</Button>
+    </div>
+    <div class="my-8 border-2 border-rose-500 p-8">
+      <Slider label="Attack" value={0} units="samples"></Slider>
+      <Slider label="Sustain" value={0} units="samples"></Slider>
+      <Slider label="Sustain Punch" value={0} max="1" step="0.01"></Slider>
+      <Slider label="Decay" value={0} units="samples"></Slider>
+
+      <Slider label="Start Frequency" value={0} units="hz"></Slider>
+      <Slider label="Min Frequency" value={0} units="hz"></Slider>
+      <Slider label="Ramp" value={0} min="-1" max="1" step="0.01"></Slider>
+      <Slider label="DRamp" value={0} min="-1" max="1" step="0.01"></Slider>
+
+      <Slider label="Vibrato strength" value={0} max="1" step="0.01"></Slider>
+      <Slider label="Speed" value={0} max="1" step="0.01"></Slider>
+      <Slider label="Delay" value={0} max="1" step="0.01"></Slider>
+
+      <Slider label="Low Pass Filter Resonance" value={0} max="1" step="0.01"></Slider>
+      <Slider label="Frequence" value={0} units="Hz"></Slider>
+      <Slider label="Ramp" value={0} max="1" min="-1" step="0.01"></Slider>
+
+      <Slider label="High Pass Filter Frequency" value={0} units="Hz"></Slider>
+      <Slider label="Ramp" value={0} max="1" min="-1" step="0.01"></Slider>
+
+      <Slider label="Phase Offset" value={0} min="-1" max="1" step="0.01"></Slider>
+      <Slider label="Ramp" value={0} min="-1" max="1" step="0.01"></Slider>
+
+      <Slider label="Repeat Speed" value={0} max="1" step="0.01"></Slider>
+
+      <Slider label="Arpeggio Speed" value={0} max="1" step="0.01"></Slider>
+      <Slider label="Arpeggio Mod" value={0} min="-1" max="1" step="0.01"></Slider>
+    </div>
   </div>
 </div>
+
