654cd9eff476f1f30a2dd856c4284a5c4365c6c0
Jerahmeel Cosinas <contact@jerahmeelcosinas.net>
2023-12-03 04:15:59 +1100

    upgrade sveltekit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script context="module" lang="ts">
-  export const prerender = true;
+
 </script>
 
 <script lang="ts">
@@ -13,7 +13,7 @@
 
 <div class="text-white bg-gray-900 min-h-screen flex flex-col justify-center items-center">
   <div class="w-full max-w-screen-lg p-4 flex justify-center flex-col items-center">
-    <img class="" src={logo} width="150" height="80" />
+    <img alt="square wave" class="" src={logo} width="150" height="80" />
     <h1 class="mb-8 text-2xl text-center w-full">Sound Effect Generator</h1>
 
     <div class="flex flex-col gap-4">
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,10 +1,6 @@
-<script context="module" lang="ts">
-  export const prerender = true;
-</script>
-
 <script lang="ts">
-  import logo from '../assets/logo.svg';
-  import SoundEffectGenerator from '../components/SoundEffectGenerator.svelte';
+  import logo from '../../assets/logo.svg';
+  import SoundEffectGenerator from '../../components/SoundEffectGenerator.svelte';
 </script>
 
 <svelte:head>
@@ -13,7 +9,7 @@
 
 <div class="text-white bg-gray-900 min-h-screen flex flex-col justify-center items-center">
   <div class="w-full max-w-screen-lg p-4 flex justify-center flex-col items-center">
-    <img class="" src={logo} width="150" height="80" />
+    <img alt="square wave" class="" src={logo} width="150" height="80" />
     <h1 class="mb-8 text-2xl text-center w-full">Sound Effect Generator</h1>
     <div class="flex flex-col gap-4">
       <SoundEffectGenerator />
